e8bb38e093680dd81c4579416bb316f9dae6e048
David Ledger <davidledger@live.com.au>
2017-12-06 16:32:13 +1100

    Base

--- EEVblog Phone Calulator/EEVblog_Phone_Calulator/MainPage.xaml.cs
+++ EEVblog Phone Calulator/EEVblog_Phone_Calulator/MainPage.xaml.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms;
-
+//By David Ledger 2017
 namespace EEVblog_Phone_Calulator
 {
     public partial class MainPage : ContentPage
